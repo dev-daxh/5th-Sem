6c18b41679e2a6c4609e9b7a6ed4f67cc7dbc57a
dev_daxh <dakshtitarmare2006@gmail.com>
2024-12-05 21:04:33 +0530

    intial

--- SemPractice/Unit3/src/ShapesAWT.java
+++ SemPractice/Unit3/src/ShapesAWT.java
@@ -1,48 +1,31 @@
 import java.awt.*;
 import java.awt.event.*;
-
-public class ShapesAWT extends Frame {
-
-    // Constructor to set up the frame
-    public ShapesAWT() {
-        // Set the title of the frame
-        setTitle("AWT Rectangle Drawing");
-
-        // Set the size of the frame
-        setSize(400, 400);
-
-        // Set the frame visibility to true
+import java.util.concurrent.Flow;
+public class ShapesAWT extends Frame{
+    ShapesAWT()
+    {
+        setLayout(new FlowLayout(2));
+        setSize(500,500);
         setVisible(true);
-        
-        // Add a window listener to handle the close button
+
         addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent we) {
-                System.exit(0); // Exit the program when the window is closed
+            public void windowClosing(WindowEvent we){
+                dispose();
             }
         });
     }
+    public void paint(Graphics g){
+        g.setColor(Color.red);
+        g.fillRect(50, 50, 200, 100);
 
-    // Override the paint method to draw a rectangle
-    public void paint(Graphics g) {
-       // Set the color for the rectangle
-       g.setColor(Color.BLUE);
-       // Draw a rectangle with top-left corner at (50, 50) and width 300, height 200
-       g.fillRect(50, 50, 300, 200);
-       // Set the color for the square
-       g.setColor(Color.GREEN);
-       // Draw a square with top-left corner at (100, 150) and side length 100
-       g.fillRect(100, 150, 100, 100); // A square has equal width and height
-       
-       // Set the color for the circle
-       g.setColor(Color.RED);
-       // Draw a filled circle at (200, 200) with radius 50
-       g.fillOval(200, 200, 100, 100); // Width and height are both 100 for a perfect circle
+        g.setColor(Color.blue);
+        g.fillRect(150, 200, 100, 100);
 
+        g.setColor(Color.yellow);
+        g.fillOval(300, 300, 100, 100);
     }
-
-    // Main method to create the frame
     public static void main(String[] args) {
-        // Create an instance of the RectangleDrawing class
         new ShapesAWT();
     }
+
 }
--- SemPractice/Unit3/src/SmileyFace.java
+++ SemPractice/Unit3/src/SmileyFace.java
@@ -1,50 +1,27 @@
 import java.awt.*;
 import java.awt.event.*;
 
-public class SmileyFace extends Frame {
-
-    // Constructor to set up the frame
-    public SmileyFace() {
-        // Set the title of the frame
-        setTitle("AWT Smiley Face");
-
-        // Set the size of the frame
-        setSize(400, 400);
-
-        // Set the frame visibility to true
+public class SmileyFace extends Frame{
+    SmileyFace(){
+        setLayout(new FlowLayout());
+        setSize(400,400);
+        setTitle("AWT ");
         setVisible(true);
-        
-        // Add a window listener to handle the close button
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent we) {
-                System.exit(0); // Exit the program when the window is closed
-            }
-        });
+
     }
+    public void paint(Graphics g){
+        g.setColor(Color.yellow);
+        g.fillOval(50, 50, 300, 300);
 
-    // Override the paint method to draw the smiley face
-    public void paint(Graphics g) {
-        // Set the color for the face (Yellow)
-        g.setColor(Color.YELLOW);
-        // Draw the face (a large circle)
-        g.fillOval(100, 100, 200, 200);
+        g.setColor(Color.black);
+        g.fillOval(150, 150, 40, 40);
+        g.fillOval(220, 150, 40, 40);
 
-        // Set the color for the eyes (Black)
-        g.setColor(Color.BLACK);
-        // Draw the left eye
-        g.fillOval(150, 150, 30, 30);
-        // Draw the right eye
-        g.fillOval(220, 150, 30, 30);
+        g.drawArc(150, 200, 100, 50, 0, -180);
 
-        // Set the color for the smile (Black)
-        g.setColor(Color.BLACK);
-        // Draw the smile using an arc
-        g.drawArc(150, 200, 100, 50, 0, -180); // x, y, width, height, startAngle, arcAngle
+
     }
-
-    // Main method to create the frame
     public static void main(String[] args) {
-        // Create an instance of the SmileyFace class
         new SmileyFace();
     }
 }
